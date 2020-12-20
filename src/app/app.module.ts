ddf8ece8b9005f3bfff1938cfd3fc184a4040da8
Gregory Reyes <greyes3@udi.edu.co>
2020-12-20 17:21:48 -0500

    Angular Routing Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,22 +12,33 @@ import { AppComponent } from './app.component';
 import { from } from 'rxjs';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
-
-
-import { DishService } from './services/dish.service';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MatToolbarModule,
     MatListModule,
     MatGridListModule,
